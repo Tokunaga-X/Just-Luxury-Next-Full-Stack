b059196f2434be0d2b6f8f330622293aa86f536d
Tokunaga-X <officialxyc@gmail.com>
2024-09-11 00:31:09 +0800

    chore: using new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,14 @@ import Footer from "./components/Footer"
 import Header from "./components/Header"
 import { Inter } from "next/font/google"
 import type { Metadata } from "next"
+import { Montserrat } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
+const poppins = Montserrat({
+    weight: ["400", "600", "700"],
+    subsets: ["latin"],
+    display: "swap",
+})
 
 export const metadata: Metadata = {
     title: "Just-Luxury",
@@ -19,7 +25,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="zh" className="h-full">
-            <body className={`${inter.className} flex flex-col h-screen`}>
+            <body className={`${poppins.className} flex flex-col h-screen`}>
                 <Header />
                 <main className="flex-1">{children}</main>
                 <Footer />
